cad11682f8bde566d0f3cee3fb7a2c283c2a62d2
Christophe Dehais <christophe.dehais@gmail.com>
2022-11-16 17:51:20 +0100

    Clean up after investigation of the set_required_version() bug

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, GLArea, Inhibit};
-use gtk::gdk::GLContext;
+use gtk::{Application, ApplicationWindow, GLArea, Inhibit, Widget, Window};
+use gtk::gdk::{GLContext, GLError};
+use gtk::gdk;
+use gtk::glib::error::Error;
+use std::ffi::{CStr, c_char};
 
 const APP_ID: &str = "goodartistscopy.Gull";
 
@@ -34,19 +37,41 @@ fn build_ui(app: &Application) {
     let gl_canvas = GLArea::builder()
         .auto_render(false)
         .build();
-    gl_canvas.set_required_version(4, 6);
+
+    gl_canvas.connect_create_context(|canvas| {
+        let context = gdk::Display::default().and_then(|display| { display.create_gl_context().ok() })?;
+        
+        context.set_required_version(4, 6);
+        context.set_forward_compatible(true);
+        context.set_debug_enabled(true);
+
+        if let Err(error) = context.realize()
+        {
+            canvas.set_error(Some(&Error::new(GLError::NotAvailable, error.message())));
+            return None;
+        }
+
+        context.make_current();
+        let (major, minor) = context.version();
+        let vendor = unsafe { CStr::from_ptr(gl::GetString(gl::VENDOR) as *const c_char) };
+        let renderer = unsafe { CStr::from_ptr(gl::GetString(gl::RENDERER) as *const c_char) }; 
+        println!("OpenGL version: {}.{} (forward compatible:{}, debug:{})", major, minor, context.is_forward_compatible(), context.is_debug_enabled());
+        println!("Vendor: {:?}\nRenderer: {:?}", vendor, renderer);
+
+        Some(context)
+    });
+
     gl_canvas.connect_render(render);
 
     window.set_child(Some(&gl_canvas));
     window.present();
 }
 
-fn render(_canvas: &GLArea, context: &GLContext) -> Inhibit {
+fn render(_canvas: &GLArea, _context: &GLContext) -> Inhibit {
     unsafe {
         gl::ClearColor(1.0, 0.0, 0.0, 1.0);
         gl::Clear(gl::COLOR_BUFFER_BIT);
     }
-    let (major, minor) = context.version();
-    println!("OpenGL version: {}.{}", major, minor);
+
     Inhibit(true)
 }
